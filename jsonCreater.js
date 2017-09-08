c5351dbf80c797bebaf0a6d9fc65c8c199ab6d80
Aleksei Kolosov <jora364@gmail.com>
2017-09-08 13:54:30 +0300

    Create JSON files

--- jsonCreater.js
+++ jsonCreater.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'h'];
+const tones = [1, 2, 4, 8, 16, 32];
 
 let jsonFile = [];
 
+genRandomTone = () => tones[Math.floor(Math.random()*6)]
 genRandomNote = () => notes[Math.floor(Math.random()*7)]
+genRandomSize = () => Math.floor(Math.random()*200)+1;
+generatedJSON = [];
+for (let i=0; i<=genRandomSize(); i++){
+    generatedJSON.push({note:genRandomNote(), tone:genRandomTone()})
+}
+fs.writeFileSync('data2.json', JSON.stringify(generatedJSON));
 
-generate = (count) => new Array(count).map(i=>genRandomNote())
-
-console.log(generate(20));
 // 1/2 1/4 1/8 1/16
